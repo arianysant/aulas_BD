71e0374dc0f012ded9a8ad7efe862b3db3d23639
Ariany Santos <100931444+arianysant@users.noreply.github.com>
2022-04-07 09:32:12 -0300

    atividades de BD

---
+++ aulaBD2.sql
@@ -0,0 +1,8 @@
+insert into pessoas(
+nome,idade,sexo,peso,altura,nacionalidade)
+values
+("Dolores",25,"f",50.62,1.52,"mexicana"),
+("Godolores",8,"f",30.25,1.12,"Americana");
+
+select * from pessoas;
+
